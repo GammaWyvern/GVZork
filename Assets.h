176953193c7a293184dc631d4b1cc6e5e0a83658
Keagen <keegenjt@gmail.com>
2023-09-25 15:38:29 -0400

    Added Class descriptions for Location and Game

--- Assets.h
+++ Assets.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 class Item {
 	public:
@@ -31,11 +32,35 @@ class NPC {
 
 class Location {
 	public:
-
+		Location(std::string name, std::string desc);
+		std::map<std::string, Location> get_locations();
+		void add_location(std::string direction, Location location);
+		void add_npc(NPC npc);
+		std::vector<NPC> get_npcs();
+		void add_item(Item item);
+		void set_visited();
+		bool get_visited();
+		friend std::ostream & operator << (std::ostream &out, const Location *location);
 	private:
-
+		std::string name;
+		std::string desc;
+		bool visited;
+		std::map<std::string, Location> neighbors;
+		std::vector<NPC> npcs;
+		std::vector<Item> items;
 };
 
 class Game {
+	public:
 
+	private:
+		//Command = alias for function -> void (std::vector<std::string>)
+		//map<std::string, Command>
+		std::vector<Item> playerItems;
+		int playerWeight;
+		std::vector<Location> locations;
+		Location playerLocation;
+		int elfHunger;
+		bool gameInProgress;
 };
+
